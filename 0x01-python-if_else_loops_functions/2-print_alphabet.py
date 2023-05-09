2a7d219dbe8c885bcad584d14e77144980e946ac
Edwinogechi <edwinogechi96@gmail.com>
2023-05-09 10:51:24 -0700

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 
 """Print alphabet in lowercase, not followed by a new line."""
 for chars in range(97, 122 + 1):
-    print("{}".format(chr(chars)),end="")
+    print("{}".format(chr(chars)), end="")
