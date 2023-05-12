7e347773b367af6aa006e2d2825175263f2b0707
Edwinogechi <edwinogechi96@gmail.com>
2023-05-12 01:46:00 -0700

    hot fix

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import dis
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -8,5 +10,5 @@ def magic_calculation(a, b):
         return(c)
     else:
         return(sub(a, b))
-import dis
+
 dis.dis(magic_calculation)
