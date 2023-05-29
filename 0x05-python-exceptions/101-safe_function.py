88857e3d96f5e1731ee19684a93fef61026f6b0d
Edwinogechi <edwinogechi96@gmail.com>
2023-05-29 10:54:09 -0700

    Hot fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import sys
-
-
 def safe_function(fct, *args):
+
+    from sys import stderr
+
     """Executes a function correctly.
 
     Args:
@@ -16,11 +16,12 @@ def safe_function(fct, *args):
     """
 
     try:
-
+        # Execute the function with the provided arguments
         outcome = fct(*args)
 
         return (outcome)
 
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    except Exception as message:
+        # Print the Exception message to stderr
+        print("Exception:", message, file=stderr)
         return (None)
