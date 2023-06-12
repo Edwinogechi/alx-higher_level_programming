50a960133096ae4aa22bc9a4a0a4147dc755068c
Edwinogechi <edwinogechi96@gmail.com>
2023-06-12 09:15:51 -0700

    A function that writes a class Rectangle that inherits from BaseGeometry(7-base_geometry.py)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
-"""A module that defines an empty class BaseGeometry."""
-
-
-class BaseGeometry:
-    """An empty class that represent base geometry."""
-
-    def area(self):
-        """The area not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name associated with the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""A module that defines a class BaseGeometry inherited ."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """A class defining a rectangle using B """
+
     def __init__(self, width, height):
         """Instantiates class object Rectangle
 
