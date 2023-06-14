523d6e7b00ff724dc8c3560d798554887e778b31
Edwinogechi <edwinogechi96@gmail.com>
2023-06-14 08:20:56 -0700

    A function that writes a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines a class MyInt that inherits int"""
+
+
+class MyInt(int):
+    """Invert equality operators on int"""
+    def __eq__(self, value):
+        """Override equal if not equal"""
+        if self is not value:
+            return True
+        else:
+            return False
+
+    def __ne__(self, value):
+        """Override not equal if equal"""
+        if self is value:
+            return True
+        else:
+            return False
