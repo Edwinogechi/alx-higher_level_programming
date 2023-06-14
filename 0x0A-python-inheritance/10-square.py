0814d10c510cdc577f526a324ebeecb75b0659b0
Edwinogechi <edwinogechi96@gmail.com>
2023-06-14 08:02:23 -0700

    A function that writes a class that inherits from a given instance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -35,5 +35,6 @@ class Square(Rectangle):
         Arguments:
             size (int): The size of the new square.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
