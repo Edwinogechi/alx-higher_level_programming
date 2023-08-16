51bdc17b8edf87ed23c708de1a207421eb68a548
Edwinogechi <edwinogechi96@gmail.com>
2023-08-16 01:07:51 -0700

    A script that prints a square

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---A script that creates a table called first_table in MYSQL server.
+-- A script that creates a table called first_table in MYSQL server.
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
