a937727f34cec87bcc589bb1beef476253624444
Edwinogechi <edwinogechi96@gmail.com>
2023-08-18 02:47:33 -0700

    A scsript tha finds the second largest int in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,17 +2,16 @@
 
 // Check if the number of arguments passed via the command line is 3 or less
 if (process.argv.length <= 3) {
-    // In case of insufficient arguments, print 0
-    console.log(0);
+  // In case of insufficient arguments, print 0
+  console.log(0);
 } else {
-    // Convert the provided command-line arguments into an array of numbers,
-    // then exclude the first two elements (executable and script names),
-    // and finally, sort the numbers in ascending order
-    const args = process.argv.map(Number)
-        .slice(2, process.argv.length)
-        .sort((a, b) => a - b);
+  // Convert the provided command-line arguments into an array of numbers,
+  // then exclude the first two elements (executable and script names),
+  // and finally, sort the numbers in ascending order
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
 
-    // Display the second-to-last (penultimate) number from the sorted array
-    console.log(args[args.length - 2]);
+  // Display the second-to-last (penultimate) number from the sorted array
+  console.log(args[args.length - 2]);
 }
-
