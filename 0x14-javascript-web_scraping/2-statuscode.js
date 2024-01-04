be7707942ce6999c7a8b7ca781bc2ccc3e2b4619
Edwinogechi <edwinogechi96@gmail.com>
2024-01-04 07:05:28 -0800

    a script that display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 
+// Import the 'request' module for making HTTP requests
 const request = require('request');
-// Import the 'request' module for making HTTP requests.
 
-request.get(process.argv[2])
-// Initiate an HTTP GET request to the specified URL using the 'request' module.
+// Retrieve the URL from the command-line arguments
+const url = process.argv[2];
 
-  .on('response', function (response) {
-    // Set up an event listener for the 'response' event emitted by the HTTP request.
-
-    console.log(`Status Code: ${response.statusCode}`);
-    // Log the HTTP status code of the response to the console.
+// Perform an HTTP GET request to the specified URL using the 'request' module
+request
+  .get(url)
+  // Set up an event listener for the 'response' event emitted by the HTTP request
+  .on('response', (response) => {
+    // Log the HTTP status code of the response to the console
+    console.log('Status Code: ' + response.statusCode);
   });
