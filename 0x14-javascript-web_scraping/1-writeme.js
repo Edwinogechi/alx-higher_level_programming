3b7a32a4b776c122f4926950f3d01162e816844d
Edwinogechi <edwinogechi96@gmail.com>
2024-01-04 06:11:16 -0800

    a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,10 +8,10 @@ const filePath = process.argv[2];
 const writeData = process.argv[3];
 
 // Write data to the specified file asynchronously
-fs.writeFile(filePath, writeData, 'utf-8', (error) => {
+fs.writeFile(filePath, writeData, error => {
   // Check if there is an error during file writing
   if (error) {
-    console.log(error); // Log the error to the console
+    console.log(error);  // Log the error to the console
   } else {
     console.log(`Data written to ${filePath}`); // Log a success message
   }
